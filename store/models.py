c5ac9cc671c080c27ee5495c93c905c29930fa2b
Armin <armin2003sheibak@gmail.com>
2024-05-19 16:08:56 +0330

    create store app and designing data model

---
+++ store/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+
+
+class Customer(models.Model):
+    MEMBERSHIP_CHOICES = [("G", "Gold"), ("S", "Silver"), ("B", "Bronze")]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default="B")
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_CHOICES = [
+        ("P", "Pending"),
+        ("C", "Complete"),
+        ("F", "Failed"),
+    ]
+
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default="P"
+    )
+    placed_at = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
--- storefront/settings.py
+++ storefront/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     # local
     "accounts",
+    "store",
 ]
 
 MIDDLEWARE = [
