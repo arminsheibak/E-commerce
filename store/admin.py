6e87b45bebc09d501410f098c304c249ca1accb1
Armin <armin2003sheibak@gmail.com>
2024-05-19 18:07:37 +0330

    customize admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Product)
+class PorductAdmin(admin.ModelAdmin):
+    list_display = ["title", "unit_price", "inventory_status"]
+    list_editable = ["unit_price"]
+    list_per_page = 10
+
+    @admin.display(ordering="inventory")
+    def inventory_status(self, product: models.Product):
+        if product.inventory < 10:
+            return "Low"
+        return "OK"
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "membership"]
+    list_editable = ["membership"]
+    list_per_page = 10
+    search_fields = ["first_name__istartswith", "last_name__istartswith"]
+
+
+admin.site.register(models.Collection)
+
+
+class OrderItemInline(admin.StackedInline):
+    model = models.OrderItem
+    extra = 0
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "placed_at", "customer"]
+    inlines = [OrderItemInline]
--- store/models.py
+++ store/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Collection(models.Model):
     title = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -12,6 +15,9 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
 
 
 class Customer(models.Model):
@@ -22,6 +28,9 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default="B")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Address(models.Model):
