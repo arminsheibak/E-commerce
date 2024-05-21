ef154fc6a8ff7b9cc1193ac7278d4279d4e5054a
Armin <armin2003sheibak@gmail.com>
2024-05-21 13:37:07 +0330

    Cart API

--- store/models.py
+++ store/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from uuid import uuid4
 
 
 class Collection(models.Model):
@@ -68,10 +70,14 @@ class OrderItem(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+
+    class Meta:
+        unique_together = [["cart", "product"]]
--- store/urls.py
+++ store/urls.py
@@ -5,12 +5,17 @@ from . import views
 router = routers.SimpleRouter()
 router.register("products", views.ProductViewSet)
 router.register("collections", views.CollectionViewSet)
+router.register("carts", views.CartViewSet)
 
 products_router = routers.NestedSimpleRouter(router, "products", lookup="product")
 products_router.register("reviews", views.ReviewViewSet, basename="product-reviews")
+
+carts_router = routers.NestedSimpleRouter(router, "carts", lookup="cart")
+carts_router.register("items", views.CartItemViewSet, basename="cart-items")
 
 
 urlpatterns = [
     path("", include(router.urls)),
     path("", include(products_router.urls)),
+    path("", include(carts_router.urls)),
 ]
